b27e940198f57fd5283b73a97005b05a506734f3
gracebir <grace.devolop@gmail.com>
2023-03-26 00:50:42 +0200

    remote dummy data

--- base/views.py
+++ base/views.py
@@ -1,20 +1,19 @@
 from django.shortcuts import render
 # Create your views here.
+from .models import Room
 
-rooms = [
-    {'id': 1, 'name': 'Lets learn python'},
-    {'id': 2, 'name': 'Design with me'},
-     {'id': 3, 'name': 'front end developer'},
-]
+# rooms = [
+#     {'id': 1, 'name': 'Lets learn python'},
+#     {'id': 2, 'name': 'Design with me'},
+#      {'id': 3, 'name': 'front end developer'},
+# ]
 
 def home(request):
+    rooms = Room.objects.all()
     context = {'rooms': rooms}
     return render(request, 'base/home.html',context)
 
 def room(request, pk):
-    room = None
-    for i in rooms:
-        if i['id'] == int(pk):
-            room = i
+    room = Room.objects.get(id=pk)
     context = {'room':room}
     return render(request, 'base/room.html', context)
