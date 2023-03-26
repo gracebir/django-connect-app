6627e525f3f89d2c7a60a6c5682040f776450181
gracebir <grace.devolop@gmail.com>
2023-03-26 22:50:49 +0200

    crud functionallity

--- base/views.py
+++ base/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 # Create your views here.
 from .models import Room
-
+from .form import RoomForm
 # rooms = [
 #     {'id': 1, 'name': 'Lets learn python'},
 #     {'id': 2, 'name': 'Design with me'},
@@ -17,3 +17,31 @@ def room(request, pk):
     room = Room.objects.get(id=pk)
     context = {'room':room}
     return render(request, 'base/room.html', context)
+
+def createRoom(request):
+    form = RoomForm()
+    if request.method == 'POST':
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    context = {'form': form}
+    return render(request, 'base/room_form.html', context)
+
+def updateRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    form = RoomForm(instance=room)
+    if request.method == 'POST':
+        form = RoomForm(request.POST, instance=room)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    context = {'form': form}
+    return render(request, 'base/room_form.html', context)
+
+def deleteRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    if request.method == 'POST':
+        room.delete()
+        return redirect('home')
+    return render(request, 'base/delete_form.html', {'obj':room})
