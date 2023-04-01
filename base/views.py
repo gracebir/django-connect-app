349e6d160b0a702a6d7f95306ccfa2f9fd26d2f0
gracebir <grace.devolop@gmail.com>
2023-04-01 20:51:29 +0200

    user profile and static file

--- base/views.py
+++ base/views.py
@@ -71,6 +71,20 @@ def home(request):
     return render(request, 'base/home.html',context)
 
 
+def profilePage(request, pk):
+    user = User.objects.get(id=pk)
+    rooms = user.room_set.all()
+    room_messages = user.message_set.all()
+    topics = Topic.objects.all()
+    context = {
+        'user':user, 
+        'rooms':rooms, 
+        'room_messages': room_messages, 
+        'topics': topics
+        }
+    return render(request, 'base/profile.html', context)
+
+
 def room(request, pk):
     room = Room.objects.get(id=pk)
     room_messages = room.message_set.all().order_by('-created')
@@ -94,7 +108,9 @@ def createRoom(request):
     if request.method == 'POST':
         form = RoomForm(request.POST)
         if form.is_valid():
-            form.save()
+            room = form.save(commit=False)
+            room.host = request.user
+            room.save()
             return redirect('home')
     context = {'form': form}
     return render(request, 'base/room_form.html', context)
